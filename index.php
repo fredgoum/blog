6034b0330a7c82f7e6d7b05473bfcc514b4889b1
fredgoum <alfredgoumou@gmail.com>
2021-05-24 18:23:57 +0200

    organize the project into folders

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!-- Router calls the correct Controller -->
 <?php
-require('controller.php');
+require('./controller/frontend.php');
 
 if (isset($_GET['action'])) {
   if ($_GET['action'] == 'listPosts') {
