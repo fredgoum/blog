556ecec42fd98e06c91fad088ea7afa962c6897a
fredgoum <alfredgoumou@gmail.com>
2021-05-24 17:30:38 +0200

    create page template for views

--- indexView.php
+++ indexView.php
@@ -1,35 +1,30 @@
 <!-- View displays infos in HTML page -->
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8"/>
-    <title>Mon blog</title>
-    <link href="./css/style.css" rel="stylesheet" /> 
-  </head>
+<?php $title = 'Mon blog'; ?>
 
-  <body>
-    <h1>Mon super blog !</h1>
-    <p>Derniers billets du blog :</p>
+<?php ob_start(); ?>
+<h1>Mon super blog !</h1>
+<p>Derniers billets du blog :</p>
 
-    <?php
-    while ($data = $posts->fetch())
-    {
-    ?>
-      <div class="news">
-        <h3>
-          <?php echo htmlspecialchars($data['title']) ?>
-          <em>le <?php echo $data['creation_date_fr']; ?></em>
-        </h3>
-        
-        <p>
-          <?php echo nl2br(htmlspecialchars($data['content'])) ?>
-          <br/>
-          <em><a href="post.php?id=<?php echo $data['id']; ?>">Commentaires</a></em>
-        </p>
-      </div>
-    <?php
-    }
-    $posts->closeCursor();
-    ?>
-  </body>
-</html>
+<?php
+while ($data = $posts->fetch())
+{
+?>
+  <div class="news">
+    <h3>
+      <?php echo htmlspecialchars($data['title']) ?>
+      <em>le <?php echo $data['creation_date_fr']; ?></em>
+    </h3>
+    
+    <p>
+      <?php echo nl2br(htmlspecialchars($data['content'])) ?>
+      <br/>
+      <em><a href="post.php?id=<?php echo $data['id']; ?>">Commentaires</a></em>
+    </p>
+  </div>
+<?php
+}
+$posts->closeCursor();
+?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
--- postView.php
+++ postView.php
@@ -1,34 +1,29 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8"/>
-    <title>Mon blog</title>
-    <link href="./css/style.css" rel="stylesheet"/>
-  </head>
+<?php $title = 'Mon blog'; ?>
 
-  <body>
-    <h1>Mon super blog !</h1>
-    <p><a href="index.php">Retour à la liste des billets</a></p>
+<?php ob_start(); ?>
+<h1>Mon super blog !</h1>
+<p><a href="index.php">Retour à la liste des billets</a></p>
 
-    <div class="news">
-      <h3>
-        <?= htmlspecialchars($post['title']) ?>
-        <em>le <?= $post['creation_date_fr'] ?></em>
-      </h3>
-      <p>
-        <?= nl2br(htmlspecialchars($post['content'])) ?>
-      </p>
-    </div>
+<div class="news">
+  <h3>
+    <?= htmlspecialchars($post['title']) ?>
+    <em>le <?= $post['creation_date_fr'] ?></em>
+  </h3>
+  <p>
+    <?= nl2br(htmlspecialchars($post['content'])) ?>
+  </p>
+</div>
 
-    <h2>Commentaires</h2>
-    <?php
-    while ($comment = $comments->fetch())
-    {
-    ?>
-      <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
-      <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
-    <?php
-    }
-    ?>
-  </body>
-</html>
+<h2>Commentaires</h2>
+<?php
+while ($comment = $comments->fetch())
+{
+?>
+  <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
+  <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+<?php
+}
+?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
