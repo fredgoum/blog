e975eddd4a62d8d6761cef121f14d0645727d6d5
fredgoum <alfredgoumou@gmail.com>
2021-05-29 17:02:08 +0200

    add comments

--- model/frontend.php
+++ model/frontend.php
@@ -13,6 +13,15 @@ function dbConnect()
 	}
 }
 
+function postComment($postId, $author, $comment)
+{
+	$db = dbConnect();
+	$comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+	$affectedLines = $comments->execute(array($postId, $author, $comment));
+
+	return $affectedLines;
+}
+
 function getPosts()
 {
 	$db = dbConnect();
--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -15,7 +15,24 @@
 </div>
 
 <h2>Commentaires</h2>
+
+<!-- Comments form -->
+<form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+  <div>
+    <label for="author">Auteur</label><br />
+    <input type="text" id="author" name="author" />
+  </div>
+  <div>
+    <label for="comment">Commentaire</label><br />
+    <textarea id="comment" name="comment"></textarea>
+  </div>
+  <div>
+    <input type="submit" />
+  </div>
+</form>
+
 <?php
+// Display Post Comments
 while ($comment = $comments->fetch())
 {
 ?>
